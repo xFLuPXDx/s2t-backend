060ad4cc3877e17618603ffc3f6277419c1b7063
Rohit Gurav <117719818+xFLuPXDx@users.noreply.github.com>
2024-01-08 23:18:59 +0530

    insert and join group working

--- models/model.py
+++ models/model.py
@@ -5,7 +5,7 @@ class Users(BaseModel):
     user_Fname : str
     user_Lname : str
     user_Email : str
-    group_Ids : list | None = [""]
+    group_Ids : list | None = []
     user_Type : str
     hashed_password : str
     
@@ -13,11 +13,11 @@ class Groups(BaseModel):
     group_Id : str
     group_Name : str
     group_Subject : str
-    educator_Ids : list | None = [""]
-    learner_Ids : list | None = [""]
+    educator_Ids : list | None = []
+    learner_Ids : list | None = []
 
 class Resources(BaseModel):
     resource_Id : str
     lecture_Transcript : str
     summarized_Text  : str
-    resource_links : list | None = [""]
+    resource_links : list | None = []
--- routes/groupRoute.py
+++ routes/groupRoute.py
@@ -2,28 +2,39 @@ from typing import Annotated
 from fastapi import APIRouter, Depends
 from auth import TokenData, get_current_active_user
 from config.db import group_collection , user_collection
-from models.model import  Groups, Users 
+from models.model import  Groups
 from schemas.users import get_groups , get_group, get_user 
 
 groupRouter = APIRouter()
 
-@groupRouter.get('/getGroup')
-async def fetch_group(current_user: Annotated[TokenData, Depends(get_current_active_user)]):
-    cursor = get_user( user_collection.find_one({ 'user_Email' : current_user.user_Email}))
-    groupIds = cursor["group_Ids"]
-    return groupIds
-
-
 @groupRouter.post('/insertGroup')
 async def insert_group(group : Groups , current_user: Annotated[TokenData, Depends(get_current_active_user)]):
-    group_collection.insert_one(dict(group))
-    return "Inserted data successfully"
+    user_cursor = get_user(user_collection.find_one({ 'user_Email' : current_user.user_Email}))
+    if user_cursor["user_Type"] == "educator" :
+        group_collection.insert_one(dict(group))
+        return user_cursor["user_Type"] + " Created group"
+    
+    return user_cursor["user_Type"]
 
-@groupRouter.get('/getSpecific_Group/{gid}')
-def getSpecific_Group(gid : str):
-    return get_group(group_collection.find_one({'group_Id' : gid}))
+@groupRouter.get('/fetchGroups')
+async def fetch_group(current_user: Annotated[TokenData, Depends(get_current_active_user)]):
+    user_cursor = get_user(user_collection.find_one({ 'user_Email' : current_user.user_Email}))
+    groupIds = user_cursor["group_Ids"]
+    grps = list()
+    if groupIds != [] :
+        for gid in groupIds :
+            grps.append(get_group(group_collection.find_one({"group_Id" : gid})))
+            return grps
+        
+    return False
 
 
-
-
-
+@groupRouter.post('/joinGroup')
+async def join_group(code : str , current_user: Annotated[TokenData, Depends(get_current_active_user)]):
+    user_collection.find_one_and_update({'user_Email' : current_user.user_Email},{"$push" : { "group_Ids" : code }})
+    user_cursor = get_user(user_collection.find_one({ 'user_Email' : current_user.user_Email}))
+    if user_cursor["user_Type"] == "educator" : 
+        group_collection.find_one_and_update({'group_Id' : code},{"$push" : {"educator_Ids" : user_cursor["user_Id"]}})
+    else:
+        group_collection.find_one_and_update({'group_Id' : code},{"$push" : {"learner_Ids" : user_cursor["user_Id"]}})
+    return True
